6a70e3e098b2d04009eae2dec752ce6f44a39ae2
brennan-v- <brennan-v-@users.noreply.github.com>
2015-12-22 21:35:19 -0800

    Trimmed imports

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from flask import Flask, request, session, redirect, url_for, abort, \
      render_template, flash
 from dataset import connect
 
